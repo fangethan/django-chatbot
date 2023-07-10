91f0ee1c6c6718b8b9ecfaa363e03b0aa189a24c
Ethan Fang <ethan.fang@gmail.com>
2023-07-11 01:51:22 +1000

    Reformat code to black standard

--- chatbot/views.py
+++ chatbot/views.py
@@ -8,17 +8,19 @@ import openai
 openai_api_key = get_openai_api_key()
 openai.api_key = openai_api_key
 
+
 def ask_openai(message):
     response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = message, 
-        max_tokens = 150,
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
         n=1,
         stop=None,
         temperature=0.7,
     )
     answer = response.choices[0].text.strip()
     return answer
+
 
 # Create your views here.
 def chatbot(request):
